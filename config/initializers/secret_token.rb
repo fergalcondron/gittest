b6adfca5d93528d31abe111ba9d9b935f43c03a6
Fergal Condron <fergal.condron@ucdconnect.ie>
2012-02-24 22:14:48 +0000

    'firstcommit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tester::Application.config.secret_token = '0db3cd13ff51ce16aba0937e24a7b3257d08a16d9c2485a2cb5f73ad711bb5333d83fe096029fd2ae3034832caeadd0d862a3fca23168ca65adca7bdf69a5ece'
